bf00ad098c33781b63dc68019b489293cbebe283
Kool Havis <havisk@gmail.com>
2017-03-16 18:23:47 -0400

    finished

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,22 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  animations: [
+    trigger('showField', [
+      state('show', style({opacity: '1'})),
+      state('hide', style({opacity: '0'})),
+      transition('show <=> hide', [
+        animate(500)
+        ])
+      ])
+  ]
 })
 export class ProfileComponent implements OnInit {
   firstName;
   lastName;
-  email;
+  emails;
+  newEmail;
+  errMsg;
+  pattern;
+  state: string = 'hide';
 
   constructor() { 
-    this.firstName = 'Joe';
-    this.lastName = 'Doe';
-    this.email = ['Select Email'];
+    this.firstName = '';
+    this.lastName = '';
+    this.emails = ['Select Email'];
+
+  
+  }
+
+  togglestates(){
+    this.state = (this.state === 'hide' ? 'show' : 'hide');
   }
 
   ngOnInit() {
+    
   }
-
+  addEmail(){
+    if(!this.newEmail) {
+      this.errMsg = "Please Enter Valid Email Address!!!";
+    }
+    else{
+      this.emails.push(this.newEmail);
+      this.newEmail = "";
+      this.errMsg = "";
+      this.state = (this.state === 'hide' ? 'show' : 'hide');
+    }   
+  }
 }
